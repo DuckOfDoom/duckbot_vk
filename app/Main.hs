9b92ecf182a5ca9e4c939f5515b4b79b1c539108
Vladimir Yanovsky <horaxlfa@gmail.com>
2018-08-07 10:49:51 +0300

    Moved some stuff from main

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,7 @@
 module Main where
 
-import           Control.Monad.Reader
-import           Data.Text            as T
-
-import           Bot                  (start)
-import           Bot.Config           (Config)
-import           Bot.Types            (Env (..))
-
-import           Data.Aeson           (decodeFileStrict)
-
+import           Bot                  (startBot)
 import           BotPrelude
 
 main :: IO ()
-main = do
-  initEnv >>= runReaderT start
-
-initEnv :: IO Env
-initEnv = do
-  config' <- readConfig
-  pure $ Env
-   { _config = config'
-   , _logger = putStrLn . T.unpack
-   }
-
-readConfig :: IO Config
-readConfig = do
-  c <- decodeFileStrict "config.json"
-  case c of
-    Nothing   -> die $ "Can't find config file config.json"
-    Just conf -> pure conf
+main = startBot
--- src/Bot.hs
+++ src/Bot.hs
@@ -1,16 +1,41 @@
 module Bot
-    (
-    start
-    ) where
+  ( startBot
+  ) where
 
 import           Bot.Types            (Bot, config)
 import           Control.Lens         ((^.))
 import           Control.Monad.Reader (ask)
 import           BotPrelude
 import           Utils.Logging        (logInfo)
+import           Control.Monad.Reader
+import           Data.Text            as T
 
-start :: Bot ()
-start = do
-  logInfo "Starting..."
-  env <- ask
-  logInfo $ showT $ env ^. config
+import           Bot.Config           (Config)
+import           Bot.Types            (Env (..))
+
+import           Data.Aeson           (decodeFileStrict)
+
+startBot :: IO ()
+startBot = do
+  env <- initEnv
+  runReaderT start env
+    where
+     start = do
+      logInfo "Starting..."
+      e <- ask
+      logInfo $ showT $ e ^. config
+
+initEnv :: IO Env
+initEnv = do
+  config' <- readConfig
+  pure $ Env
+   { _config = config'
+   , _logger = putStrLn . T.unpack
+   }
+
+readConfig :: IO Config
+readConfig = do
+  c <- decodeFileStrict "config.json"
+  case c of
+    Nothing   -> die $ "Can't find config file config.json"
+    Just conf -> pure conf
