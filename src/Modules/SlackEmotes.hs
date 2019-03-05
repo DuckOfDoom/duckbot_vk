995bc2f01a491223c5829a90d49b683e29c13cdb
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-03-05 18:01:12 +0300

    wip parser

--- src/Modules/SlackEmotes.hs
+++ src/Modules/SlackEmotes.hs
@@ -4,10 +4,21 @@ where
 
 import BotPrelude 
 
-import Data.Attoparsec.Text (Parser, parseTest)
+import Data.Attoparsec.Text (Parser, char, parseTest, many1, satisfy, inClass, endOfInput, string, space)
+import Data.Text as T
 
-test :: Text -> IO ()
-test t = parseTest parser t 
+test :: IO ()
+test = parseTest parser "\"asd\" :monkas: :whats_up:"
 
 parser :: Parser Text
-parser = undefined
+parser = do
+  let someText = many1 (satisfy (inClass "A-Za-z"))
+  let emoteName = sequence [string ":", T.pack <$> many1 (satisfy (inClass "a-z_")), string ":"]
+
+  text <- char '\"' *> someText <* char '\"'
+  _ <- space
+  emote1 <- emoteName
+  _ <- space
+  emote2 <- emoteName
+  -- endOfInput
+  pure $ showT (text, emote1, emote2)
