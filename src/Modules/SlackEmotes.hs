49df510b630a7e6e2aba58b59b6d437d33dda07c
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-03-06 19:09:46 +0300

    Wip

--- src/Modules/SlackEmotes.hs
+++ src/Modules/SlackEmotes.hs
@@ -1,32 +1,35 @@
 module Modules.SlackEmotes 
 ( parser
+, inputParser
 , zipLetters
 )
 where
 
+import Bot.Types  (Bot)
+
 import BotPrelude 
 
-import Data.Attoparsec.Text (Parser, char, parseTest, many1, satisfy, inClass, endOfInput, string, space, (<?>))
+import Data.Attoparsec.Text (Parser, parseTest, many1, satisfy, inClass, string, space, (<?>))
 import Data.Text as T
 
-test :: IO ()
-test = parseTest parser "asd :monkas: :whats_up:" 
+parser :: Parser (Integer -> Bot ())
+parser = undefined
 
-parser :: Parser Text
-parser = do
+inputParser :: Parser (Text, Text, Text)
+inputParser = do
   let someText = many1 (satisfy (inClass "A-Za-z")) <?> "Only letters!"
   let emoteName = mconcat <$> sequence [string ":", T.pack <$> many1 (satisfy (inClass "a-z_")), string ":"] <?> "Letters with '_' inside colons!"
-
+  
   text <- someText 
   _ <- space
   emote1 <- emoteName
   _ <- space
   emote2 <- emoteName
-  pure $ showT (text, emote1, emote2)
+  pure (text, emote1, emote2)
 
 zipLetters :: [[Text]] -> [[Text]] -> [[Text]] 
 zipLetters (x:xs) (y:ys) = (x ++ [" "] ++ y) : zipLetters xs ys
-zipLetters [] [] = []
+zipLetters _ _ = []
 
 -- mkLetter :: Char -> [Text]
 -- mkLetter = 
