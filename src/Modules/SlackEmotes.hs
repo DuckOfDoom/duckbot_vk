ff1c248f593a9b5c43b3cca3f85b2d428057655b
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-03-06 19:15:08 +0300

    Fixed compilation

--- src/Modules/SlackEmotes.hs
+++ src/Modules/SlackEmotes.hs
@@ -9,7 +9,7 @@ import Bot.Types  (Bot)
 
 import BotPrelude 
 
-import Data.Attoparsec.Text (Parser, parseTest, many1, satisfy, inClass, string, space, (<?>))
+import Data.Attoparsec.Text (Parser, many1, satisfy, inClass, string, space, (<?>))
 import Data.Text as T
 
 parser :: Parser (Integer -> Bot ())
@@ -17,7 +17,7 @@ parser = undefined
 
 inputParser :: Parser (Text, Text, Text)
 inputParser = do
-  let someText = many1 (satisfy (inClass "A-Za-z")) <?> "Only letters!"
+  let someText = T.pack <$> many1 (satisfy (inClass "A-Za-z")) <?> "Only letters!"
   let emoteName = mconcat <$> sequence [string ":", T.pack <$> many1 (satisfy (inClass "a-z_")), string ":"] <?> "Letters with '_' inside colons!"
   
   text <- someText 
