185bb83e125341cbd60fd8d632bc2a9d636a4f49
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-03-10 01:22:10 +0300

    Refactored tests, added all letters, fixed parsing with spaces

--- src/Modules/SlackEmotes/Internal.hs
+++ src/Modules/SlackEmotes/Internal.hs
@@ -6,40 +6,50 @@ module Modules.SlackEmotes.Internal
   , zipLetters
   ) where
 
-import BotPrelude hiding (option, empty)
+import BotPrelude hiding (Word, option, empty)
 
-import Data.Attoparsec.Text (Parser, option, many1, satisfy, inClass, string, space, (<?>))
+import Data.Attoparsec.Text (Parser, manyTill, option, many1, satisfy, inClass, string, space, (<?>))
+import Data.Attoparsec.Combinator (lookAhead)
+
 import qualified Data.Text as T
 import qualified Data.Char as C
 
 import Modules.SlackEmotes.Letters (getLetter, availableLetters)
 
+type Letter = [[Text]]
+type Word = [[Text]]
+
 inputParser :: Parser (Text, Text, Text)
 inputParser = do
-  let someText = T.pack <$> many1 (satisfy (inClass (availableLetters ++ map C.toUpper availableLetters))) <?> "Input. someText"
   let emoteName = mconcat <$> sequence [string ":", T.pack <$> many1 (satisfy (inClass "a-z_")), string ":"] <?> "Input. emoteName"
   let defaultEmptyEmote = ":white_small_square:"
-  
-  text <- someText 
+  -- Parse any text consisting of available letter and spaces until we get to " :" (start of the emote)
+  text <- manyTill (satisfy (inClass (availableLetters ++ map C.toUpper availableLetters)) <|> space) (lookAhead $ string " :")
+  -- Skip space and parse an emote name
   filled <- space *> emoteName
+  -- Second emote is optional, so we insert white square if there is no second emote.
   empty <- option defaultEmptyEmote (space *> emoteName)
-  pure (T.toLower text, filled, empty)
+  pure ((T.toLower . T.pack) text, filled, empty)
 
-render :: [[Text]] -> Text
+-- Render lists as text
+render :: Word -> Text
 render = T.unlines . map mconcat
 
-mkLetter :: Text -> Text -> Char -> [[Text]]
-mkLetter filled empty c = (replaceChars filled empty (getLetter c))
+-- Make a letter by substituting emotes in lists
+mkLetter :: Text -> Text -> Char -> Letter
+mkLetter filled empty c = (replaceChars (getLetter c) filled empty)
 
-zipLetters :: Text -> [[[Text]]] -> [[Text]]
+-- Zip two letters together with a separator
+zipLetters :: Text -> [Letter] -> Letter
 zipLetters separator = fold0 zip2 
   where
-    zip2 :: [[Text]] -> [[Text]] -> [[Text]] 
+    zip2 :: Letter -> Letter -> Letter 
     zip2 (x:xs) (y:ys) = (x ++ [separator] ++ y) : zip2 xs ys
     zip2 _ _ = []
 
-replaceChars :: Text -> Text -> [[Text]] -> [[Text]]
-replaceChars filled empty = (map . map) replaceChars'
+-- Replace # and spaces in l with 
+replaceChars :: Letter -> Text -> Text -> Letter
+replaceChars letter filled empty = (map . map) replaceChars' letter
   where
     replaceChars' i = 
       case i of 
--- test/SlackEmotesTests.hs
+++ test/SlackEmotesTests.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE FlexibleInstances #-}
+-- Guess i really need that instance
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module SlackEmotesTests
 ( spec ) where
 
@@ -5,8 +9,18 @@ import BotPrelude
 import Test.Hspec
 
 import Modules.SlackEmotes.Internal (replaceChars, zipLetters, inputParser)
-import Data.Attoparsec.Text (parseOnly)
+import Data.Attoparsec.Text (parse, parseOnly, IResult(..))
 import qualified Data.Text as T
+
+instance Eq a => Eq (IResult Text a) where
+  (Done i1 r1) == (Done i2 r2) = i1 == i2 && r1 == r2
+  (Fail i1 c1 e1) == (Fail i2 c2 e2) = i1 == i2 && c1 == c2 && e1 == e2
+
+  -- These two are added so that Parial results can be fully evaluated for testing
+  x == (Partial f) = x == f ""
+  (Partial f) == x = f "" == x
+
+  _ == _ = False
 
 spec :: Spec
 spec = describe "Module: SlackEmotes" $ do
@@ -18,11 +32,11 @@ inputParsing :: Spec
 inputParsing = 
   describe "inputParser" $ do
     it "Parses input correctly" $ do
-      let input = "ab"
-      parseOnly inputParser (input <> " :monkas: :hey_yo:") `shouldBe` Right (input, ":monkas:", ":hey_yo:")
+      let input = "The quick brown fox jumps over the lazy dog"
+      parse inputParser (input <> " :monkas: :hey_yo:") `shouldBe` Done "" (T.toLower input, ":monkas:", ":hey_yo:")
       -- should treat any case as lower (we have only one alphabet anyways)
-      parseOnly inputParser (T.toUpper input <> " :monkas: :hey_yo:") `shouldBe` Right (input, ":monkas:", ":hey_yo:")
-      parseOnly inputParser "a :monkas:" `shouldBe` Right ("a", ":monkas:", ":white_small_square:")
+      parse inputParser (T.toUpper input <> " :monkas: :hey_yo:") `shouldBe` Done "" (T.toLower input, ":monkas:", ":hey_yo:")
+      parse inputParser "a :monkas:" `shouldBe` Done "" ("a", ":monkas:", ":white_small_square:")
     it "Does not parse input with special symbols" $ 
       parseOnly inputParser "text text2" `shouldSatisfy` isLeft
     it "Does not parse incorrect input" $ do
@@ -35,10 +49,10 @@ replacingCharacters :: Spec
 replacingCharacters = 
   describe "replaceChars" $ do
     it "Replaces characters correctly" $ 
-      replaceChars "hey" "yo" [[" ", "#", "#"], ["#", " ", " "]] `shouldBe` [["yo", "hey", "hey"], ["hey", "yo", "yo"]]
+      replaceChars [[" ", "#", "#"], ["#", " ", " "]] "hey" "yo" `shouldBe` [["yo", "hey", "hey"], ["hey", "yo", "yo"]]
     it "Leaves incorrect chars as is" $ 
       let input = [["1", "2", "3"], ["1", "2", "3"]] in
-      replaceChars "hey" "yo" input `shouldBe` input
+      replaceChars input "hey" "yo" `shouldBe` input
 
 zippingLetters :: Spec
 zippingLetters =
